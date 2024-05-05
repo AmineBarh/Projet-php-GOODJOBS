64ce554d0a4bd307ebcc8b6ca585d6b1849eeae9
Mohamed Amine Barhoumi <mohamed.amine.barhoumi.eng@gmail.com>
2024-05-05 18:16:04 +0100

    fine2

--- delete.php
+++ delete.php
@@ -1,28 +1,14 @@
 <?php
-session_start(); // Start the session
-
-// Retrieve the ID to delete
+session_start();
 $id = $_GET['id'];
-
-// Include the database connection file
 include "coonexion.php";
-
-// Delete the user's record
 $stmt = $cnx->prepare("DELETE FROM webuser WHERE id = ?");
 $stmt->execute([$id]);
-
-// Check if the deletion was successful
 if ($stmt->rowCount() > 0) {
-    // Destroy the session
     session_destroy();
-    // Redirect to the login page
     header('Location: login.php');
     exit;
 } else {
-    // If deletion fails, handle the error or redirect accordingly
     echo "Error: Unable to delete user record.";
-    // Redirect to an appropriate page
-    // header('Location: error.php');
-    // exit;
 }
 ?>
